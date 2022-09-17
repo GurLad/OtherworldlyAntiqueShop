d4f7207d1a9da1876ff4664258ad402f8239b4d6
Mehmet Koray Yılmaz <korpik123456@gmail.com>
2022-09-17 15:41:39 +0300

    item and ghost item

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public bool corrupted;
+    Camera maincam;
+    SpriteRenderer sr;
+    Vector3 realpos;
+    public float conveyorSpeed;
+    bool dragging;
+    SpriteRenderer ghost;
+    public SpriteRenderer ghostobject;
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        maincam = Camera.main;
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (!dragging)
+        {
+            transform.position += new Vector3( Time.fixedDeltaTime * conveyorSpeed,0,0);
+
+        }
+        else
+        {
+            ghost.transform.position += new Vector3(Time.fixedDeltaTime * conveyorSpeed, 0, 0);
+
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        ghost = Instantiate(ghostobject,transform.position,Quaternion.identity);
+        ghost.sprite = sr.sprite;
+        dragging = true;
+
+    }
+    private void OnMouseDrag()
+    {
+
+        Vector3 pos = maincam.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0;
+        transform.position = pos;
+
+
+
+    }
+
+    private void OnMouseUp()
+    {
+        transform.position = ghost.transform.position;
+        Destroy(ghost);
+        dragging = false;
+
+        //if mouse up on nothing return to previous position + movement (show movement with a translucent copy)
+    }
+
+
+    public void BuyEffect()
+    {
+        if (corrupted)
+        {
+            //bad
+        }
+        else
+        {
+            //good
+        }
+    }
+
+    public void RejectEffect()
+    {
+        //animations and stuff yee
+    }
+
+  
+}
